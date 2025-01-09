a5f01209d343ff1597abe229fc28d919089e8011
sonirad <sonya48694907@gmail.com>
2025-01-10 01:21:48 +0900

    총알 표시 ui 추가

--- FPS/Assets/Scripts/Player/Player.cs
+++ FPS/Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 using StarterAssets;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class Player : MonoBehaviour
     [Tooltip("기본 총(리볼버)")]
     private GunBase defaultGun;
 
+    [Tooltip("총이 변경되었음을 알리는 델리게이트")]
+    public Action<GunBase> onGunChange;
+
     private void Awake()
     {
         starterAssets = GetComponent<StarterAssetsInputs>();
@@ -27,11 +31,6 @@ public class Player : MonoBehaviour
         // 모든 총 찾기
         guns = child.GetComponentsInChildren<GunBase>(true);
 
-        foreach (GunBase gun in guns)
-        {
-            gun.onFire += controller.FireRecoil;
-        }
-
         // 기본 총
         defaultGun = guns[0];
     }
@@ -40,10 +39,22 @@ public class Player : MonoBehaviour
     {
         starterAssets.onZoom += DisableGunCamera;      // 줌 할 떄 실행될 함수 연결
 
+        CrossHair crossHair = FindAnyObjectByType<CrossHair>();
+
+        foreach (GunBase gun in guns)
+        {
+            // 화면 튕기는 효과
+            gun.onFire += controller.FireRecoil;
+            // 조준선 확장 효과
+            gun.onFire += (expend) => crossHair.Expend(expend * 10);
+        }
+
         // 기본 총 설정
         activeGun = defaultGun;
         // 기본 총 장비
         activeGun.Equip();
+        // 총 변경 알림
+        onGunChange?.Invoke(activeGun);
     }
 
     /// <summary>
@@ -69,10 +80,12 @@ public class Player : MonoBehaviour
         activeGun = guns[(int)gunType];
         activeGun.Equip();
         activeGun.gameObject.SetActive(true);
+        // 총 변경 알림
+        onGunChange?.Invoke(activeGun);
     }
 
     /// <summary>
-    /// 장비 중인 총을 발사. 아직 미완성
+    /// 장비 중인 총을 발사.
     /// </summary>
     /// <param name="isFireStart">t : 발사버튼 눌렀다, f : 발사버튼 땠다.</param>
     public void GunFire(bool isFireStart)
@@ -80,13 +93,29 @@ public class Player : MonoBehaviour
         activeGun.Fire(isFireStart);
     }
 
+    /// <summary>
+    /// 리볼러를 재장전
+    /// </summary>
     public void RevolverReload()
     {
         Revolver revolver = activeGun as Revolver;
 
+        // activeGun이 리볼버 일때만 재장전
         if (revolver != null)
         {
             revolver.ReLoad();
+        }
+    }
+
+    /// <summary>
+    /// 총알 갯수가 변경될 때 실행되는 델리게이트에 콜백 추가
+    /// </summary>
+    /// <param name="callback">추가할 콜백 함수</param>
+    public void AddBulletCountChangeDelegate(Action<int> callback)
+    {
+        foreach (var gun in guns)
+        {
+            gun.onBulletCountChange += callback;
         }
     }
 }
--- FPS/Assets/Scripts/UI/BulletCount.cs
+++ FPS/Assets/Scripts/UI/BulletCount.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BulletCount : MonoBehaviour
 {
+    private TextMeshProUGUI current;
+    private TextMeshProUGUI max;
 
+    private void Awake()
+    {
+        Transform child = transform.GetChild(0);
+        current = child.GetComponent<TextMeshProUGUI>();
+        child = transform.GetChild(2);
+        max = child.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        Player player = GameManager.Instance.Player;
+
+        player.AddBulletCountChangeDelegate(OnBulletCountChane);
+        player.onGunChange += OnGunChange;
+    }
+
+    private void OnBulletCountChane(int count)
+    {
+        current.text = count.ToString();
+    }
+
+    private void OnGunChange(GunBase gun)
+    {
+        max.text = gun.clipSize.ToString();
+    }
 }
