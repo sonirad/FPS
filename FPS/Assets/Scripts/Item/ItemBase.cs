ea440a464beee5295a0ff26a50d3aa9262629b79
sonirad <sonya48694907@gmail.com>
2025-01-13 17:38:48 +0900

    총기 아이템 작성 중

---
+++ FPS/Assets/Scripts/Item/ItemBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    [Tooltip("1초에 회전하는 속도")]
+    public float spinSpeed = 360.0f;
+    [Tooltip("회전시킬 메시의 트랜스폼")]
+    private Transform meshTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    /// <summary>
+    /// 아이템을 먹었을 때 실행되는 함수
+    /// </summary>
+    /// <param name="player">아이템을 먹은 플레이어</param>
+    protected virtual void OnItemConsum(Player player)
+    {
+
+    }
+}
